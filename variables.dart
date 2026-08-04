64a74281459eba0a50978f5fb92ca26e1e2cfcbd
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-04 12:05:50 +0545

    Add variable example

---
+++ variables.dart
@@ -0,0 +1,16 @@
+// Exercise 1
+// Create variables for:
+// Product Name
+// Product Price
+// Quantity
+// Calculate the total cost.
+
+void main() {
+  String productName = "Face Wash";
+  double productPrice = 700.00;
+  int quantity = 2;
+
+  double totalCost = productPrice * quantity;
+
+  print("Total Cost of $quantity $productName is Rs. $totalCost");
+}
