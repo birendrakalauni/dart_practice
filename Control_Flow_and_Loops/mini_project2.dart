54964a15f8f4a3f7cc40a1633ca732fd48fa24bb
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-05 23:46:09 +0545

    Added mini project(switch case used)

---
+++ Control_Flow_and_Loops/mini_project2.dart
@@ -0,0 +1,21 @@
+//Restaurant Ordering System
+void main() {
+  int menuChoice = 3;
+
+  switch (menuChoice) {
+    case 1:
+      print("Pizza");
+      break;
+    case 2:
+      print("Burger");
+      break;
+    case 3:
+      print("Momo");
+      break;
+    case 4:
+      print("Chowmein");
+      break;
+    default:
+      print("Invalid Menu Selection");
+  }
+}
