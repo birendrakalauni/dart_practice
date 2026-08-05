84ad2c92c6b4ba581e46da4c37af6d0656319553
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-05 16:35:02 +0545

    Added mini project:Student Profile

---
+++ Introduction_to_Dart/mini_project1.dart
@@ -0,0 +1,15 @@
+void main() {
+  const schoolName = "SNSC";
+  const country = "Nepal";
+  final registrationDate = DateTime.now();
+  var semester = "6th";
+
+  print("School Name: $schoolName");
+  print("Country: $country");
+  print("Resistration Time: $registrationDate");
+  print("Semester: $semester");
+
+  semester = "7th";
+  print("\nAfter Semester Update: ");
+  print("Smester: $semester");
+}
