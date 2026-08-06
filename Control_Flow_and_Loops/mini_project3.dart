d4754e307edbfd62761c313f96b769d547818191
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-06 22:30:23 +0545

    Modify to count how many students scored 80 or above

--- Control_Flow_and_Loops/mini_project3.dart
+++ Control_Flow_and_Loops/mini_project3.dart
@@ -8,12 +8,13 @@ void main() {
   int total = 0;
   int highest = marks[0];
   int lowest = marks[0];
+  int count8OOrAbove = 0;
 
   for (int mark in marks) {
     print(mark);
 
     total += mark; //Calculate total
-    
+
     //Check for the highest mark
     if (mark > highest) {
       highest = mark;
@@ -22,6 +23,9 @@ void main() {
     if (mark < lowest) {
       lowest = mark;
     }
+    if (mark >= 80) {
+      count8OOrAbove++;
+    }
   }
   double average = total / marks.length;
 
@@ -29,4 +33,5 @@ void main() {
   print("Average : $average");
   print("Highest : $highest");
   print("Lowest : $lowest");
+  print("Students scoring 80 or above : $count8OOrAbove");
 }
