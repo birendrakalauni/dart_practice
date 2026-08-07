0d75f025f77467fc66c71848f0c5b7d04ce6ab70
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-07 11:14:19 +0545

    Added folder name Functions with mini project: Student Information System

---
+++ Functions/mini_project1.dart
@@ -0,0 +1,26 @@
+// Student Information System
+void printHeader() {
+  print("==========================");
+  print("Student Information System");
+  print("==========================");
+  print("");
+}
+
+void printStudentDetails() {
+  print("Name: Birendra");
+  print("Age: 22");
+  print("SNSC");
+  print("");
+}
+
+void printFooter() {
+  print("=========================");
+  print("Thank You!");
+  print("=========================");
+}
+
+void main() {
+  printHeader();
+  printStudentDetails();
+  printFooter();
+}
