6e7eb2df2f2e2f979e3aadbbee031bbe85595e87
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-07 12:09:21 +0545

    Added mini project: Employee Salary Slip

--- Functions/mini_project2.dart
+++ Functions/mini_project2.dart
@@ -7,11 +7,15 @@ void salarySlip(String name, String position, double salary) {
   print("Name: $name");
   print("Position: $position");
   print("BAsec Salary: Rs. $salary");
+
   double tax = salary * 10 / 100;
   double netSalary = salary - tax;
+
   print("Tax(10%): Rs. $tax");
   print("Net Salary: Rs. $netSalary");
   print("");
+  print("======================");
+  print('');
 }
 
 void main() {
