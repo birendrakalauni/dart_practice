b1d35107605f718b5c513c273d80f6e8e27b4221
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-16 00:00:09 +0545

    Added oexamples of operators

---
+++ Learning_with_BW/operators.dart
@@ -0,0 +1,10 @@
+// Different Types of operators
+
+//Arithmetic Operators
+void main() {
+  int a = 10;
+  int b = 5;
+  print("Addition: ${a + b}");
+  print("Subtract: ${a - b}");
+  print("Multiplication: ${a * b}");
+}
