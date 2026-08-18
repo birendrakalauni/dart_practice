9262633504039ec2e680abc4526d7c29835efbdc
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-18 23:58:26 +0545

    Added simple examples of if_else statement

---
+++ Learning_with_BW/if_else.dart
@@ -0,0 +1,17 @@
+void main() {
+  int age = 18;
+  if (age >= 18) {
+    print("You can vote!");
+  } else {
+    print("You can't vote..");
+  }
+
+  int marks = 90;
+  if (marks >= 80) {
+    print("Grade A+");
+  } else if (marks >= 40) {
+    print("Grade C+");
+  } else {
+    print("Grade F");
+  }
+}
