b07c04d934a1986eddf811983fcecd1dda3e4d79
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-20 22:48:18 +0545

    Added simple example of switch

---
+++ Learning_with_BW/switch.dart
@@ -0,0 +1,19 @@
+void main() {
+  int day = 1;
+  switch (day) {
+    case 1:
+      print("Sunday");
+      break;
+
+    case 2:
+      print("Monday");
+      break;
+
+    case 3:
+      print("Tuesday");
+      break;
+
+    default:
+      print("Invalid day");
+  }
+}
