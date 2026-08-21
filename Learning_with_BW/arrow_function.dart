2ae7e1ceabe0af66e104637bb8852ceeea80bf2f
Birendra Kalauni <birendrakalauni33@gmail.com>
2026-08-21 23:07:31 +0545

    Added basic examples of arrow functions

---
+++ Learning_with_BW/arrow_function.dart
@@ -0,0 +1,10 @@
+// import 'basic_function.dart';
+
+void main() {
+  var total = add(10, 10);
+  print("Total: $total");
+  sayHello();
+}
+
+int add(int a, int b) => a + b;
+String sayHello() => "Hello";
